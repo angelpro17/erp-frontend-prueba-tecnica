10afc8cefb2e10c2443525ae8ebc1a29d5a44b65
Luis Angel Anampa Lavado <u202218664@upc.edu.pe>
2025-08-14 13:33:32 -0500

    feat(auth): login simulado con JWT mock y guard; interceptor añade Authorization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { authGuard } from "./core/guards/auth.guard";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/login",
+    pathMatch: "full",
+  },
+  {
+    path: "login",
+    loadComponent: () =>
+      import("./features/auth/components/login/login.component").then(
+        (m) => m.LoginComponent
+      ),
+    title: "Iniciar Sesión",
+  },
+
+  {
+    path: "**",
+    redirectTo: "/login",
+  },
+];
